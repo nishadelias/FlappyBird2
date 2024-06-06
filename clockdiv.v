d0cef1d340c1fd90e2e8d5d03080cdf4667ae6fe
nishadelias <123427128+nishadelias@users.noreply.github.com>
2024-06-05 17:47:26 -0700

    Update clockdiv.v

--- clockdiv.v
+++ clockdiv.v
@@ -23,7 +23,8 @@ module clockdiv(
 	input wire clr,		//asynchronous reset
 	output wire dclk,		//pixel clock: 25MHz
 	output wire segclk,	//7-segment clock: 381.47Hz
-	output wire gclk   // about 50Hz
+	output wire gclk,   // about 50Hz
+	output wire blink_clk
 	);
 
 // 17-bit counter variable
@@ -48,5 +49,6 @@ assign segclk = q[17];
 // 100Mhz ÷ 4 = 25MHz --bottom 2 bits will count from 0 to 4
 assign dclk = q[0] & q[1];
 assign gclk = q[23];
+assign blink_clk = q[25];
 
 endmodule
